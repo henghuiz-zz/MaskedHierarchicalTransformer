83541aa56105619e1d81bbe5b40088c60a6debaf
Zhu <henghui@amazon.com>
2020-02-05 15:13:57 -0500

    update dataset and generation script.

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Masked Hierarchical Transformer for Conversation Structure Modeling
 
-Placeholder for datasets for paper *Who Did They Respond to? Conversation Structure Modeling Using Masked Hierarchical Transformer* by Henghui Zhu, Feng Nan, Zhiguo Wang, Ramesh Nallapati, and Bing Xiang in AAAI 2020.
+This repo contains the scripts that crawl the reddit dataset used in paper [*Who Did They Respond to? Conversation Structure Modeling Using Masked Hierarchical Transformer*](https://arxiv.org/abs/1911.10666) by Henghui Zhu, Feng Nan, Zhiguo Wang, Ramesh Nallapati, and Bing Xiang in AAAI 2020.
 
-The dataset and the scripts to process the dataet will be released in early 2020
+In order to get the dataset, one needs to register an application in Reddit and get clinet ID and screte. The details can be found [here](https://praw.readthedocs.io/en/latest/getting_started/authentication.html#password-flow). 
+
+Then clone the repo using the following command.
+```bash
+git clone https://github.com/henghuiz/MaskedHierarchicalTransformer.git
+```
+
+Next, run the following command for generating dataset in the paper.
+```bash
+cd MaskedHierarchicalTransformer
+unzip raw_data.zip
+pip install -r requirements.txt
+python generate_dataset.py --client_id=YOUR_CLIENT_ID --client_secret=YOUR_CLIENT_SECRET
+```
+By default, the dataset will be obtained in the folder `raw_data_with_text`.
+
+If you have used the datasets, please cite the following paper:
+
+```
+@article{zhu2019did,
+  title={Who did They Respond to? Conversation Structure Modeling using Masked Hierarchical Transformer},
+  author={Zhu, Henghui and Nan, Feng and Wang, Zhiguo and Nallapati, Ramesh and Xiang, Bing},
+  journal={arXiv preprint arXiv:1911.10666},
+  year={2019}
+}
+```
---
+++ generate_dataset.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import praw
+import os
+import argparse
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--client_id", type=str)
+parser.add_argument("--client_secret", type=str)
+parser.add_argument("--user_agent", type=str, default='get body text for the comment')
+parser.add_argument("--output_dir", type=str, default='raw_data_with_text')
+
+args = parser.parse_args()
+
+reddit = praw.Reddit(client_id=args.client_id, client_secret=args.client_secret, user_agent='next_sentence_prediction')
+
+
+def get_response(reddit_id):
+  result = None
+  if reddit_id.startswith('t1'):
+    try:
+      comment = reddit.comment(id=reddit_id[3:])
+      result = comment.body
+    except:
+      pass
+
+  elif reddit_id.startswith('t3'):
+    try:
+      comment = reddit.submission(id=reddit_id[3:])
+      result = comment.title
+    except:
+      pass
+
+  return result
+
+
+def build_dataset(input_csv, output_csv, dry_run=False):
+  reddit_df = pd.read_csv(input_csv)
+  print(len(reddit_df))
+  if dry_run:
+    reddit_df = reddit_df[:100]
+  reddit_ids = reddit_df['id'].values
+
+  all_text = []
+
+  for reddit_id in tqdm(reddit_ids):
+    instance_text = get_response(reddit_id)
+    all_text.append(instance_text)
+
+  reddit_df['body'] = all_text
+  reddit_df.to_csv(output_csv, index=False)
+
+
+def main():
+  os.makedirs(args.output_dir, exist_ok=True)
+  train_csv_in = 'raw_data/train.csv'
+  train_csv_out = os.path.join(args.output_dir, 'train.csv')
+  build_dataset(train_csv_in, train_csv_out)
+
+  val_csv_in = 'raw_data/val.csv'
+  val_csv_out = os.path.join(args.output_dir, 'val.csv')
+  build_dataset(val_csv_in, val_csv_out)
+
+  test_csv_in = 'raw_data/test.csv'
+  test_csv_out = os.path.join(args.output_dir, 'test.csv')
+  build_dataset(test_csv_in, test_csv_out)
+
+
+if __name__ == '__main__':
+  main()
